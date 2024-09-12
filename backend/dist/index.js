76b7b23f160f7e483d15f8ef6237a3b884ebf527
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-12 14:05:07 +0530

    first commit

---
+++ backend/dist/index.js
@@ -0,0 +1,20 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+const PORT = process.env.PORT || 5001;
+connectToDatabase()
+    .then(() => {
+    const server = app.listen(PORT, () => console.log(`Server running on port ${PORT} and connected to the database`));
+    // Error handling for server listen
+    server.on("error", (err) => {
+        if (err.code === "EADDRINUSE") {
+            console.error(`Port ${PORT} is already in use. Trying to restart...`);
+            // Optional: Terminate the process to restart it cleanly (useful for dev environments)
+            process.exit(1);
+        }
+        else {
+            console.error("Server error:", err);
+        }
+    });
+})
+    .catch((err) => console.error("Database connection error:", err));
+//# sourceMappingURL=index.js.map
---
+++ backend/src/db/connection.ts
@@ -0,0 +1,22 @@
+import {connect, disconnect} from 'mongoose'
+
+async function connectToDatabase(){
+    try{
+        await connect(process.env.MONGODB_URL)
+    }catch (error){
+        console.log(error)
+        throw new Error("Cannot connect to MONGODB database")
+    }
+
+}
+
+async function disconnectFromDatabase() {
+    try {
+        await disconnect
+    } catch (error) {
+        console.log(error)
+        throw new Error("Cannot connect to MONGODB database")
+    }
+}
+
+export {connectToDatabase, disconnectFromDatabase}
