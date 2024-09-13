6733efde698b97f109c451a6a9c22f4f121b9b56
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-13 13:55:01 +0530

    Adding new frontend changes

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
+
+function Header() {
+  const auth = useAuth();
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo></Logo>
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Goto Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="Logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
