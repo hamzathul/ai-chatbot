fa008f383cdb886b6a09b56131ab9db5e14910c7
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-16 21:23:05 +0530

    deployment setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,8 +9,9 @@ config(); // This loads environment variables from a .env
 
 const app = express();
 
+const corsURI = process.env.NODE_ENV === "development" ? "http://localhost:5173" : ""
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); //This middleware enables Cross-Origin Resource Sharing (CORS), which allows or restricts requests from other domains
+app.use(cors({ origin: corsURI, credentials: true })); //This middleware enables Cross-Origin Resource Sharing (CORS), which allows or restricts requests from other domains
 app.use(express.json()); //parses incoming requests with JSON payloads and makes them available via req.body
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { connectToDatabase, disconnectFromDatabase } from "./db/connection.js";
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 connectToDatabase()
   .then(() => {
