e2fd27b4dbc164ce0118ff6eebca76ab4ac6ea03
Hamzathul Favas E <83232962+hamzathul@users.noreply.github.com>
2024-10-18 15:09:02 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ config(); // This loads environment variables from a .env
 
 const app = express();
 
-const corsURI = process.env.NODE_ENV === "development" ? "http://localhost:5173" : ""
+const corsURI = process.env.NODE_ENV === "development" ? "http://localhost:5173" : "https://ai-chatbot-frontend-0j3f.onrender.com"
 //middlewares
 app.use(cors({ origin: corsURI, credentials: true })); //This middleware enables Cross-Origin Resource Sharing (CORS), which allows or restricts requests from other domains
 app.use(express.json()); //parses incoming requests with JSON payloads and makes them available via req.body
