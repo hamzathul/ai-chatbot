6afda5842451b7c66e82636ad6df1ebb1376b235
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-22 09:50:09 +0530

    deployment setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import axios from "axios";
 import { Toaster } from "react-hot-toast";
 
 const clientURL =
-  import.meta.env.VITE_NODE_ENV === "development"
+  import.meta.env.MODE === "development"
     ? "http://localhost:5000/api/v1"
     : "https://ai-chatbot-zq3j.onrender.com/api/v1";
 
